296680fd521487bea5a9ae669bb1de4e0a17a8c2
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-12-12 14:57:34 +0100

    Fetching Data ('openweather.com')

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+
 
 function App() {
+
 	const [lat, setLat] = useState([]);
 	const [long, setLong] = useState([]);
+	const [data, setData] = useState([]);
 	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-		})
-	})
+		const fetchData = async () => {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				setLat(position.coords.latitude);
+				setLong(position.coords.longitude)
+			})
+			await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+			.then(res => res.json())
+			.then(result => {
+				setData(result)
+				console.log(result)
+			});
+
+		}
+		fetchData();
+	}, [lat, long]);
 
 	return (
 		<div className="App">
-			f
+
 		</div>
 	);
 }
